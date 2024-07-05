2f9370046666f41e345045b813343cd9f9cda077
adambaj123 <abaj1@stetson.edu>
2024-07-05 12:00:02 -0400

    Started 3D Render Practice
    1 triangle and 1 square 2D
    
    Triangle: Different Colors Per Vertices
    
    Square: One Color On Each Vertices

---
+++ 3DRendererCSharp/3DRendererCSharp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Numerics;
+using Tao.FreeGlut;
+using OpenGL;
+namespace _3DRendererCSharp
+{
+    class Program
+    {
+        private static int width = 1200, height = 720;
+        private static ShaderProgram program;
+        private static VBO<Vector3> triangle, square;
+        private static VBO<Vector3> triangleColor, squareColor;
+        private static VBO<int> triangleElements, squareElements;
+        static void Main(string[] args)
+        {
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Glut.glutInitWindowSize(width, height);
+            Glut.glutCreateWindow("OpenGL Tutorial");
+            Glut.glutIdleFunc(OnRenderFrame);
+            Glut.glutDisplayFunc(OnDisplay);
+
+            program = new ShaderProgram(VertexShader, FragmentShader);
+
+            program.Use();
+            program["projection_matrix"].SetValue(Matrix4.CreatePerspectiveFieldOfView(0.45f, (float)width / height, 0.1f, 1000f));
+            program["view_matrix"].SetValue(Matrix4.LookAt(new Vector3(0, 0, 10), Vector3.Zero, new Vector3(0,1,0)));
+
+            triangle = new VBO<Vector3>(new Vector3[] { new Vector3(0, 1, 0), new Vector3(-1, -1, 0), new Vector3(1, -1, 0 )});
+            square = new VBO<Vector3>(new Vector3[] { new Vector3(-1, 1, 0), new Vector3(-1, -1, 0), new Vector3(1, -1, 0), new Vector3(1, 1, 0) });
+
+            triangleElements = new VBO<int>(new int[] { 0, 1, 2 }, BufferTarget.ElementArrayBuffer);
+            squareElements = new VBO<int>(new int[] { 0, 1, 2, 3 }, BufferTarget.ElementArrayBuffer);
+
+            triangleColor = new VBO<Vector3>(new Vector3[] { new Vector3(1, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 0, 1) });
+            squareColor = new VBO<Vector3>(new Vector3[] { new Vector3(1, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 0), new Vector3(1, 0, 0) }); 
+            Glut.glutMainLoop();
+        }
+
+        private static void OnDisplay()
+        {
+
+        }
+
+        private static void OnRenderFrame()
+        {
+            Gl.Viewport(0, 0, width, height);
+            Gl.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            program.Use();
+            program["model_matrix"].SetValue(Matrix4.CreateTranslation(new Vector3(-1.5f, 0, 0)));
+
+            uint verexPositionIndex = (uint)Gl.GetAttribLocation(program.ProgramID, "vertexPosition");
+            Gl.EnableVertexAttribArray(verexPositionIndex);
+            Gl.BindBuffer(triangle);
+            Gl.VertexAttribPointer(verexPositionIndex, triangle.Size, triangle.PointerType, true, 12, IntPtr.Zero);
+            Gl.BindBuffer(triangleElements);
+            Gl.BindBufferToShaderAttribute(triangleColor, program, "vertexColor");
+
+
+            Gl.DrawElements(BeginMode.Triangles, triangleElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+
+            program["model_matrix"].SetValue(Matrix4.CreateTranslation(new Vector3(1.5f, 0, 0)));
+            Gl.BindBufferToShaderAttribute(square, program, "vertexPosition");
+            Gl.BindBufferToShaderAttribute(squareColor, program, "vertexColor");
+            Gl.BindBuffer(squareElements);
+
+            Gl.DrawElements(BeginMode.Quads, squareElements.Count, DrawElementsType.UnsignedInt, IntPtr.Zero);
+            Glut.glutSwapBuffers();
+        }
+
+        public static string VertexShader = @"
+in vec3 vertexPosition;
+in vec3 vertexColor;
+
+varying out vec3 color;
+
+uniform mat4 projection_matrix;
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+
+void main(void) {
+    color = vertexColor;
+    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertexPosition, 1);
+}
+";
+        public static string FragmentShader = @"
+in vec3 color;
+void main(void)
+{
+ gl_FragColor = vec4(color,1);
+}";
+    }
+
+
+}
